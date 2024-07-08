4c2309b2d0b8cea3797078aa052db14bd870231c
Ritik kumar soni <100145712+Ritik96520@users.noreply.github.com>
2024-07-08 10:07:07 +0530

    Update Contact.jsx

--- reactmultipage-main/src/Contact.jsx
+++ reactmultipage-main/src/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
       <h2 className="common-heading">Feel Free to Contact us</h2>
 
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15126.28620995241!2d73.92422475000001!3d18.59334505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c14df5c70e0d%3A0x2d19689e09e2fced!2sPhoenix%20Mall%20Washrooms!5e0!3m2!1sen!2sin!4v1658905192255!5m2!1sen!2sin"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3597.7596434576426!2d85.13868597517789!3d25.612901977445766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f2a0b995555555%3A0xced099373be97bec!2sSamriddh%20kendra!5e0!3m2!1sen!2sin!4v1720329334949!5m2!1sen!2sin"
         width="100%"
         height="450"
         style={{ border: 0 }}
